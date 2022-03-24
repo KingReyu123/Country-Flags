7a0eabfbccb9d5a2c0a3488f165981c76bd3b7f3
KingReyu123 <77984837+KingReyu123@users.noreply.github.com>
2022-03-24 10:10:41 -0700

    Add files via upload

---
+++ country_flags.py
@@ -0,0 +1,43 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import messagebox
+
+root=Tk()
+root.title("Country Flags")
+root.geometry("600x400")
+root.configure(background="lightblue")
+
+get_input = Entry(root)
+show_label = Label(root)
+
+india_map = ImageTk.PhotoImage(Image.open ("India.jpg"))
+america_map = ImageTk.PhotoImage(Image.open ("America.jpg"))
+australia_map = ImageTk.PhotoImage(Image.open ("Australia.png"))
+philippines_map = ImageTk.PhotoImage(Image.open ("Philippines.png"))
+japan_map = ImageTk.PhotoImage(Image.open ("Japan.jpg"))
+
+maps_dictionary = { "India" : india_map ,
+                   "india" : india_map,
+                    "United States" : america_map ,
+                    "united states" : america_map,
+                    "Australia" : australia_map ,
+                    "australia" : australia_map,
+                    "Philippines" : philippines_map,
+                    "philippines" : philippines_map,
+                    "Japan" : japan_map,
+                    "japan" : japan_map}
+
+def showMaps():
+    map_name = get_input.get()
+    try:
+        show_label['image']= maps_dictionary[map_name]
+    except:
+        messagebox.showinfo("Error","Sorry, this flag isn't in our system.")
+    show_label['image'] = maps_dictionary[map_name]
+
+btn = Button(root , text="Show Map", bg="green", command=showMaps)
+get_input.place(relx=0.5, rely=0.1, anchor=CENTER)
+btn.place(relx=0.5, rely=0.2, anchor=CENTER)
+show_label.place(relx=0.5, rely=0.6, anchor=CENTER)
+
+root.mainloop()
